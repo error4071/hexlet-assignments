3b619054f3dadf60e0eb19fcd08a3545e6d6d2af
@hexlet/cli <support@hexlet.io>
2023-04-13 01:14:01 +0500

    submit java-collections-ru/maps

---
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -0,0 +1,23 @@
+package exercise;
+
+public class App {
+    public static Map<String, Integer> getWordCount(String sentence) {
+
+        String[] splitSentence = sentence.split("");
+
+        Map<String, Integer> wordsCount = new HashMap<>();
+
+        for (String x: splitSentence) {
+            if (!wordsCount.containsKey(x)) {
+                wordsCount.put(x, 0);
+            }
+            return wordsCount;
+        }
+
+        public static String toString(Map<String, Integer> wordsCount) {
+            for (Map.Entry<String, Integer> entry: wordsCount.entrySet()) {
+                System.out.print("{  " + entry.getValue() + ":  " + entry.getKey() + "  }");
+            }
+        }
+    }
+}
