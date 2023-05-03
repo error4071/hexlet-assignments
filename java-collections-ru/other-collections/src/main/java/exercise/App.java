de7c5ed6be07fc825dc65d2606654aaa6a5213cd
@hexlet/cli <support@hexlet.io>
2023-05-03 12:58:36 +0500

    submit java-collections-ru/other-collections

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -3,14 +3,23 @@ package exercise;
 import java.util.*;
 
 public class App {
-    public static boolean genDiff(Map<String, Object> data1, Map<String, Object> data2) {
+    public static Map<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
 
-        Map<String, Object> data3 = new LinkedHashMap<>(data1);
-        for (int i = 0; i < data1.size(); i++) {
-            data3.add
+        Map<String, String> result = new LinkedHashMap<>();
+        Set<String> keys = new TreeSet<>(data1.keySet());
+        keys.addAll(data2.keySet());
+
+        for (String key : keys) {
+            if (!data1.containsKey(key)) {
+                result.put(key, "added");
+            } else if (!data2.containsKey(key)) {
+                result.put(key, "deleted");
+            } else if (data1.get(key).equals(data2.get(key))) {
+                result.put(key, "unchanged");
+            } else {
+                result.put(key, "changed");
+            }
         }
-
-
+            return result;
+        }
     }
-
-}
