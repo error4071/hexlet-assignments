2b75077893c771e93d90793240a7e77c65d8ff81
@hexlet/cli <support@hexlet.io>
2023-05-03 11:57:31 +0500

    submit java-collections-ru/other-collections

---
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -0,0 +1,16 @@
+package exercise;
+
+import java.util.*;
+
+public class App {
+    public static boolean genDiff(Map<String, Object> data1, Map<String, Object> data2) {
+
+        Map<String, Object> data3 = new LinkedHashMap<>(data1);
+        for (int i = 0; i < data1.size(); i++) {
+            data3.add
+        }
+
+
+    }
+
+}
