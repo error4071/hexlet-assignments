026e5b736152791a4d3acd4277ddbf22ba9494fc
@hexlet/cli <support@hexlet.io>
2023-10-15 22:09:06 +0500

    submit java-web-ru/http-session

---
+++ java-web-ru/http-session/src/main/java/exercise/App.java
@@ -0,0 +1,30 @@
+package exercise;
+
+import io.javalin.Javalin;
+import java.util.List;
+import java.util.Map;
+
+public final class App {
+
+    private static final List<Map<String, String>> USERS = Data.getUsers();
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get("/users", ctx -> {
+            var userNumber = ctx.queryParamAsClass("users", Integer.class).getOrDefault(0);
+            ctx.json(Data.getUsers());
+        });
+
+        return app;
+
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
