447b401214993efe9fa1117a1951da42ba14f787
@hexlet/cli <support@hexlet.io>
2023-11-13 22:54:15 +0500

    submit java-spring-ru/annotations

--- java-spring-ru/annotations/src/main/java/exercise/model/Address.java
+++ java-spring-ru/annotations/src/main/java/exercise/model/Address.java
@@ -1,25 +1,7 @@
 package exercise.model;
 
 import exercise.annotation.Inspect;
-
-public class Address {
-    private String city;
-    private int postalCode;
-
-    public Address(String city, int postalCode) {
-        this.city = city;
-        this.postalCode = postalCode;
-    }
-
-    @Inspect
-    public String getCity() {
-        return city;
-    }
-
-    @Inspect
-    public int getPostalCode() {
-        return postalCode;
-    }
+public record Address(@Inspect String city, @Inspect int postalCode) {
 
     public String getFullAddress() {
         return city + " " + postalCode;
