b6c2e6a31db16b251034f104dfd1a52dcc5ca62e
@hexlet/cli <support@hexlet.io>
2023-11-15 23:15:07 +0500

    submit java-spring-ru/rest-api

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -2,6 +2,8 @@ package exercise.controller.users;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,26 +12,30 @@ import exercise.model.Post;
 import exercise.Data;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 public class PostsController {
-    private List<Post> posts = Data.getPosts();
+    List<Post> posts = Data.getPosts();
 
-    @GetMapping("/users")
-    public List<Post> index(@RequestParam(defaultValue = "10") Integer limit) {
-        return posts.stream().limit(limit).toList();
+    @GetMapping("/{id}/posts")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Post> index(@PathVariable int id) {
+        return posts.stream()
+                .filter(post -> post.getUserId() == id)
+                .collect(Collectors.toList());
         }
-    @PostMapping
-    public ResponseEntity<Post> create(@RequestBody Post post) {
+
+    @PostMapping("/{id}/posts/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post create(@RequestBody Post post, @PathVariable int id) {
         Post newPost = new Post();
 
+        newPost.setUserId(id);
         newPost.setSlug(post.getSlug());
         newPost.setTitle(post.getTitle());
         newPost.setBody(post.getBody());
 
         posts.add(newPost);
 
-        URI location = URI.create("/posts");
-
-        return ResponseEntity.created(location).body(newPost);
+        return newPost;
     }
-    }
+}
