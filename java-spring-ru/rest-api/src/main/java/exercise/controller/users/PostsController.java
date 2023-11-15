a1c71c4fb01b67ba1b1f7a5c4bdbe82be8eb5361
@hexlet/cli <support@hexlet.io>
2023-11-15 23:25:47 +0500

    submit java-spring-ru/rest-api

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -1,41 +1,48 @@
 package exercise.controller.users;
 
-import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import exercise.model.Post;
 import exercise.Data;
 
+// BEGIN
 @RestController
 @RequestMapping("/api/users")
 public class PostsController {
-    List<Post> posts = Data.getPosts();
+    List<Post> repository = Data.getPosts();
 
     @GetMapping("/{id}/posts")
     @ResponseStatus(HttpStatus.OK)
     public List<Post> index(@PathVariable int id) {
-        return posts.stream()
+        return repository.stream()
                 .filter(post -> post.getUserId() == id)
                 .collect(Collectors.toList());
-        }
+    }
 
-    @PostMapping("/{id}/posts/")
+    @PostMapping("/{id}/posts")
     @ResponseStatus(HttpStatus.CREATED)
     public Post create(@RequestBody Post post, @PathVariable int id) {
         Post newPost = new Post();
 
         newPost.setUserId(id);
-        newPost.setSlug(post.getSlug());
         newPost.setTitle(post.getTitle());
         newPost.setBody(post.getBody());
+        newPost.setSlug(post.getSlug());
 
-        posts.add(newPost);
+        repository.add(newPost);
 
         return newPost;
     }
 }
+
+// END
