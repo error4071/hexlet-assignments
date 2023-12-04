98f36024daad017192bf65a24d3c86965e5393df
@hexlet/cli <support@hexlet.io>
2023-12-04 22:10:52 +0500

    submit java-spring-ru/dto

--- java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
+++ java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -22,7 +22,7 @@ public class PostsController {
     @Autowired
     private PostRepository postRepository;
 
-    @GetMapping(path = "")
+    @GetMapping(path = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public PostDTO index(@PathVariable Long id) {
         var post = postRepository.findById(id)
@@ -40,5 +40,22 @@ public class PostsController {
     @ResponseStatus(HttpStatus.OK)
     public Post show(@PathVariable Long id) {
         return postRepository.findById(id).get();
+    }
+
+    @GetMapping(path = "")
+    List<PostDTO> index() {
+        var post = postRepository.findAll();
+        var result = post.stream()
+                .map(this::toDTO)
+                .toList();
+        return result;
+    }
+
+    private PostDTO toDTO(Post post) {
+        var dto = new PostDTO();
+        dto.setId(post.getId());
+        dto.setTitle(post.getTitle());
+        dto.setBody(post.getBody());
+        return dto;
     }
 }
