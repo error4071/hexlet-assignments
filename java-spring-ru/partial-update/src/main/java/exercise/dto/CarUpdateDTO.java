57ab347e132a5f0f66e7977b774134843b78f458
@hexlet/cli <support@hexlet.io>
2024-02-24 00:46:32 +0500

    submit java-spring-ru/partial-update

--- java-spring-ru/partial-update/src/main/java/exercise/dto/CarUpdateDTO.java
+++ java-spring-ru/partial-update/src/main/java/exercise/dto/CarUpdateDTO.java
@@ -2,7 +2,6 @@ package exercise.dto;
 
 import org.openapitools.jackson.nullable.JsonNullable;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
--- java-spring-ru/partial-update/src/main/java/exercise/mapper/CarMapper.java
+++ java-spring-ru/partial-update/src/main/java/exercise/mapper/CarMapper.java
@@ -19,13 +19,9 @@ import exercise.model.Car;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 
-    public abstract class CarMapper {
-        public abstract CarDTO map(Car dto);
-        public abstract void update(CarUpdateDTO dto, @MappingTarget Car model);
-
-        public abstract Car map(CarCreateDTO dto);
-        public abstract void create(CarCreateDTO dto, @MappingTarget Car model);
-
-    }
-
+public abstract class CarMapper {
+    public abstract Car map(CarCreateDTO dto);
+    public abstract CarDTO map(Car dto);
+    public abstract void update(CarUpdateDTO dto, @MappingTarget Car model);
+}
     // END
