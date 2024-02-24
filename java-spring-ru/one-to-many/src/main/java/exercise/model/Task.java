37baff150e9d697336c350d98760333f35e8c5fb
@hexlet/cli <support@hexlet.io>
2024-02-25 00:15:54 +0500

    submit java-spring-ru/one-to-many

--- java-spring-ru/one-to-many/src/main/java/exercise/model/Task.java
+++ java-spring-ru/one-to-many/src/main/java/exercise/model/Task.java
@@ -41,7 +41,7 @@ public class Task {
     // BEGIN
     @ManyToOne
     @NotNull
-    private Long assigneeId;
+    private Long assignee;
 
     @OneToMany(mappedBy = "assignees", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Task> posts = new ArrayList<>();
