07845bd514af67d300d7e9d99b520ade15b31bb1
@hexlet/cli <support@hexlet.io>
2024-02-27 22:57:47 +0500

    submit java-spring-ru/crud

--- java-spring-ru/crud/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/crud/src/main/java/exercise/controller/ProductsController.java
@@ -9,8 +9,10 @@ import exercise.dto.ProductDTO;
 import exercise.dto.ProductUpdateDTO;
 import exercise.mapper.ProductMapper;
 import exercise.model.Product;
+import exercise.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +37,8 @@ public class ProductsController {
     private ProductMapper productMapper;
 
     // BEGIN
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -44,11 +48,15 @@ public class ProductsController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    ProductDTO create(@Valid @RequestBody ProductCreateDTO productCreateDTO) {
-        var product = productMapper.map(productCreateDTO);
-        productRepository.save(product);
-        var productDto = productMapper.map(product);
-        return productDto;
+    public ResponseEntity<ProductDTO> create(@RequestBody ProductCreateDTO productData) {
+        if (categoryRepository.findById(productData.getCategoryId()).isPresent()) {
+            var product = productMapper.map(productData);
+            productRepository.save(product);
+            var result = productMapper.map(product);
+            return ResponseEntity.status(HttpStatus.CREATED).body(result);
+        } else {
+            return ResponseEntity.badRequest().body(new ProductDTO());
+        }
     }
 
     @GetMapping("/{id}")
