1d69955914875d78831b21ae6e31fa4d4f0a09d3
@hexlet/cli <support@hexlet.io>
2024-02-28 23:29:32 +0500

    submit java-spring-ru/search

---
+++ java-spring-ru/search/src/main/java/exercise/specification/ProductSpecification.java
@@ -0,0 +1,43 @@
+package exercise.specification;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import exercise.dto.ProductParamsDTO;
+import exercise.model.Product;
+
+// BEGIN
+
+@Component
+public class ProductSpecification {
+    public Specification<Product> build(ProductParamsDTO params) {
+        return withCategoryId(params.getCategoryId())
+                .and(withPriceLt(params.getPriceLt()))
+                .and(withPriceGt(params.getPriceGt()))
+                .and(withRatingGt(params.getRatingGt()))
+                .and(withTitleCont(params.getTitleCont()));
+    }
+
+    private Specification<Product> withCategoryId(Long categoryId) {
+        return (root, query, cb) -> categoryId == null ? cb.conjunction() : cb.equal(root.get("category").get("id"), categoryId);
+    }
+
+    private Specification<Product> withPriceLt(Integer priceLt) {
+        return (root, query, cb) -> priceLt == null ? cb.conjunction() : cb.equal(root.get("price").get("lt"), priceLt);
+    }
+
+    private Specification<Product> withPriceGt(Integer priceGt) {
+        return (root, query, cb) -> priceGt == null ? cb.conjunction() : cb.equal(root.get("price").get("gt"), priceGt);
+    }
+
+    private Specification<Product> withRatingGt(Double ratingGt) {
+        return (root, query, cb) -> ratingGt == null ? cb.conjunction() : cb.equal(root.get("rating").get("gt"), ratingGt);
+    }
+
+    private Specification<Product> withTitleCont(String titleCont) {
+        return (root, query, cb) -> titleCont == null ? cb.conjunction() : cb.equal(root.get("title").get("cont"), titleCont);
+    }
+}
+
+// END
